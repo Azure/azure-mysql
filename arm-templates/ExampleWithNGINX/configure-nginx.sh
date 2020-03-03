032f6db97fba271424988a9b940bb63d5136fa6a
kummanish <manishku@microsoft.com>
2020-03-03 15:53:25 -0800

    Editing the data encryption arm template

--- arm-templates/ExampleWithNGINX/configure-nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-export DEBIAN_FRONTEND=noninteractive 
-rm /var/lib/apt/lists/* -vrf 
-apt-get -y update 
-apt-get -y install nginx 
-echo "stream { upstream postgresql { server $1.mysql.database.azure.com:3306; } server { listen 3306; proxy_pass mysql; } }" >> /etc/nginx/nginx.conf 
-service nginx restart 
-update-rc.d nginx defaults 
