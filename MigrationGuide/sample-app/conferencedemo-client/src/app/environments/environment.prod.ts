244a85a1fd6270c7654d3ac6835921bae076031f
Jim Toland <jtoland@microsoft.com>
2024-04-09 11:54:33 -0700

    remove migration guide

--- MigrationGuide/sample-app/conferencedemo-client/src/app/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    conferenceWebServerUrl: 'https://conferencedemo-onprem-mysql-to-azure-mysql.azurewebsites.net/api/v1'
-  };
-  
--- MigrationGuide/sample-app/conferencedemo/src/main/java/com/yourcompany/conferencedemo/repositories/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yourcompany.conferencedemo.repositories;
-
-import com.yourcompany.conferencedemo.models.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// You could @RespositoryRestResource to expose this interface in a RESTful manner.
-public interface EventRepository extends JpaRepository<Event, Integer> {
-
-}
